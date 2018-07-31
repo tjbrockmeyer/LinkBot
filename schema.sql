939999e4233027717a96ee72d6cd0897e55a3ca1
BrockmeyerTyler <tjb101@yahoo.com>
2018-07-31 18:16:59 -0500

    Update psql table to ready-state.

--- schema.sql
+++ schema.sql
@@ -1,35 +1,42 @@
 
+
+-- SERVER TABLES --
+
 CREATE TABLE servers (
-    server_id               bigint primary key,
+    server_id               bigint      primary key,
     entry_role              bigint
 );
 
 CREATE TABLE admins (
-    server_id               bigint references servers(server_id),
-    user_id                 bigint
+    server_id               bigint      references servers(server_id),
+    user_id                 bigint      not null,
+    primary key (server_id, user_id)
 );
 
 CREATE TABLE birthdays (
-    server_id               bigint references servers(server_id),
-    person                  text,
-    birthday                date,
-    last_congrats           integer default(0)
-);
-
-CREATE TABLE reminders (
-    remindee_id             bigint,
-    remind_at               timestamp,
-    reason                  text
+    server_id               bigint      references servers(server_id),
+    person                  text        not null,
+    birthday                date        not null,
+    last_congrats           integer,
 );
 
 CREATE TABLE quotes (
-    server_id               bigint references servers(server_id),
-    id                      integer primary key,
-    author                  text,
-    quote                   text
+    server_id               bigint      references servers(server_id),
+    id                      integer     not null,
+    author                  text        not null,
+    quote                   text        not null,
+    primary key (server_id, id)
 );
 
+CREATE TABLE reminders (
+    remindee_id             bigint      not null,
+    remind_at               timestamp   not null,
+    reason                  text
+);
+
+-- INDEPENDENT TABLES --
+
 CREATE TABLE suggestions (
-    id                      serial primary key,
-    suggestion              text unique
+    id                      serial      primary key,
+    suggestion              text        not null
 );
